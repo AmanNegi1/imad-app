24e1b64e7a6991d7f0c63e20c79339877e237d6e
AmanNegi1 <amannegi227@gmail.com>
2017-08-09 08:07:04 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.get('/article-one',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'art.html'));
 });
 app.get('/article-two',function(req,res){
-  res.sent('artical-two is called by the user');
+  res.send('artical-two is called by the user');
 });
 app.get('/articlethree',function(req,res)
 {
- res.sent('artical three is called by the user');
+ res.send('artical three is called by the user');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
